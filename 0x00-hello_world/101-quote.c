86e0e7b24438ec6221ad8ea8892263cdb34466fe
MinenhleNkosi <jetrocubaa@gmail.com>
2022-12-08 20:12:22 +0200

    Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - main method used for execution
+* Description: using the main function
+* Return: 0
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - The main method will print out what is on the puts function
+* Description: using the main function
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
